2adcf079a3c313002e506231c419aa182b39cc35
Arshita Sandhiparthi <48334673+arshita237@users.noreply.github.com>
2021-04-12 00:30:04 -0700

    Implemented Instructions toggle

---
+++ Project/Assets/Scripts/PopUp.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PopUp : MonoBehaviour
+{
+    public GameObject popUp;
+
+    public void popUpBox()
+    {
+        popUp.SetActive(true);
+    }
+
+    private void Start()
+    {
+        popUp.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            popUpBox();
+        }
+    }
+    public void closeIt()
+    {
+        popUp.SetActive(false);
+    }
+}
